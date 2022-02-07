d71b18b914440385e2cbe84aefe24059943c4fae
shrinishant <nishantkumaranaspiringiitian@gmail.com>
2022-02-07 23:23:24 +0530

    Update the first Day code to make the extension capable of scanning more than 500 contacts in the chat list : background.js

--- background.js
+++ background.js
@@ -1,5 +1,17 @@
-chrome.storage.sync.set({con_arr: ["i"]}, function() {
+chrome.storage.local.set({con_arr: ["i"]}, function() {
     console.log('Value is set to ');
 });
 
-chrome.storage.sync.set({scroll_h: 500}, function() {});
+chrome.storage.local.set({scroll_h: 500}, function() {});
+
+chrome.runtime.onConnect.addListener(function(port) {
+    if (port.name === "huzayfah") {
+        port.onDisconnect.addListener(function() {
+            chrome.storage.local.set({con_arr: ["i"]}, function() {
+                console.log('Value is set to ');
+            });
+            
+            chrome.storage.local.set({scroll_h: 500}, function() {});
+        });
+    }
+});
